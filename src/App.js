679d31662b9ef116f78774d90ea13c8d02e3f99d
Hao Zhi Yen <michaelyen@MYen.local>
2019-10-09 18:58:14 +0800

    adding starting project

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/Search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      SearchField: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({SearchField: e.target.value});
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    const {monsters, SearchField} = this.state;
+    const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(SearchField.toLowerCase()))
+    return (
+      <div className="App">
+        <SearchBox 
+          type='Search' 
+          placeholder='Search monster' 
+          handleChange = { this.handleChange }
+        />
+        <CardList monsters={filterMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
