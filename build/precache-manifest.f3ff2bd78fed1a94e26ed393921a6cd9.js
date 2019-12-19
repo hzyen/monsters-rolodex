390767546c9555caf629697d202b2daf0163b236
Hao Zhi Yen <michaelyen@MYen.local>
2019-12-20 07:36:29 +0800

    change to production

---
+++ build/precache-manifest.f3ff2bd78fed1a94e26ed393921a6cd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d423c0498ad242ae5f14254160c16455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccff88f2dc5ff5b5a090",
+    "url": "/static/css/main.d559a0c4.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "ccff88f2dc5ff5b5a090",
+    "url": "/static/js/main.47b67c3e.chunk.js"
+  },
+  {
+    "revision": "d6023e9a5195b54d38ce",
+    "url": "/static/js/runtime-main.e5eb06e0.js"
+  }
+]);
